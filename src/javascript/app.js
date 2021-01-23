6fdb73c4d908a24fc90031f2741823e18006bf40
OlamideMustapha <akinkunmimustapha1@gmail.com>
2021-01-23 17:46:51 +0100

    Updating the implementation for the pricing section

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,1 +1,15 @@
-console.log ("Hello world!")
+const ranger = document.querySelector('[type=range]');
+const pageviews = document.querySelector ("#pageviews");
+const price = document.querySelector ("#price");
+
+
+// document.documentElement.classList.add('js');
+
+ranger.addEventListener('input', e => {
+  const value = ranger.value;
+  ranger.style.setProperty('--val', +value);
+
+   
+  pageviews.innerText = value > 0 ? value + "K" : value
+  console.log (value);
+}, false);
