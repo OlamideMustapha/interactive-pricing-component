5d3e7be178e74ccdf16490f398ccf4525a2e4997
OlamideMustapha <akinkunmimustapha1@gmail.com>
2021-01-23 22:29:25 +0100

    Updating the implementation for the pricing section

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,15 +1,37 @@
-const ranger = document.querySelector('[type=range]');
+const ranger    = document.querySelector('[type=range]');
 const pageviews = document.querySelector ("#pageviews");
-const price = document.querySelector ("#price");
+const price     = document.querySelector ("#price");
+const toggle    = document.querySelector (".slider");
+const type      = document.querySelector ("#type");
 
-
-// document.documentElement.classList.add('js');
+let isYearly     = false,
+    monthlyPrice = 8,
+    yearlyPrice  = () => (monthlyPrice * 25) / 100;
+// document.ranger.classList.add('js');
 
 ranger.addEventListener('input', e => {
-  const value = ranger.value;
-  ranger.style.setProperty('--val', +value);
-
-   
-  pageviews.innerText = value > 0 ? value + "K" : value
-  console.log (value);
+  monthlyPrice = Number (ranger.value);
+  ranger.style.setProperty('--val', +ranger.value);
+  
+  pageviews.innerText = monthlyPrice === 8 ? "10K"
+                      : monthlyPrice === 12 ? "50K"
+                      : monthlyPrice === 16 ? "100K"
+                      : monthlyPrice === 20 ? "150K"
+                      : monthlyPrice === 24 ? "500K"
+                      : monthlyPrice === 28 ? "666k"
+                      : monthlyPrice === 32 ? "832K"
+                      : "1M";
+    price.innerText = isYearly 
+                    ? `$${yearlyPrice (monthlyPrice)}.00`
+                    : `$${monthlyPrice}.00`;
 }, false);
+
+toggle.addEventListener ("click", (_) => {
+  isYearly = !isYearly
+  type.innerText = isYearly ? "/ year" : "/ month";
+  price.innerText = isYearly 
+                  ? `$${yearlyPrice (monthlyPrice)}.00`
+                  : `$${monthlyPrice}.00`;
+
+  
+});
